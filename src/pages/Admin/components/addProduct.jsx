3539b2a2385256b677f01e5ec42d45a47e3d1566
dothanhDev <thanhdq2003@gmail.com>
2023-08-06 23:50:43 +0700

    update desc ckeditor

--- src/pages/Admin/components/addProduct.jsx
+++ src/pages/Admin/components/addProduct.jsx
@@ -5,6 +5,8 @@ import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 import * as Yup from "yup";
 import ProductApi from "../../../api/components/ProductApi";
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 function addProduct() {
 	// const product = useSelector((state) => state.product.productItem);
 
@@ -165,16 +167,25 @@ function addProduct() {
 				</div>
 				<div className="form-group">
 					<label htmlFor="description">description</label>
-					<textarea
-						type="text"
-						cols="50"
-						rows="15"
-						className="form-control border rounded-lg  px-2"
-						id="description"
-						name="description"
-						{...register("description")}
-					></textarea>
-					{errors.description && errors.description.message && (
+					<CKEditor
+                    editor={ ClassicEditor }
+                    data="<p>Hello from CKEditor 5!</p>"
+                    onReady={ editor => {
+                        // You can store the "editor" and use when it is needed.
+                        console.log( 'Editor is ready to use!', editor );
+                    } }
+                    onChange={ ( event, editor ) => {
+                        const data = editor.getData();
+                        console.log( { event, editor, data } );
+                    } }
+                    onBlur={ ( event, editor ) => {
+                        console.log( 'Blur.', editor );
+                    } }
+                    onFocus={ ( event, editor ) => {
+                        console.log( 'Focus.', editor );
+                    } }
+                />
+						{errors.description && errors.description.message && (
 						<div className="text-black pl-[5px] text-[14px] pt-[3px] w-full py-[4px] border-[1px] font-light bg-[#fff6f6] border-red-500 border-t-red-500 rounded-[3px] mt-[10px] ">
 							{errors.description?.message}
 						</div>
